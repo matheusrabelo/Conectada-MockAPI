06164d1c2e310d5197b827f39d26b16a4c2e5704
Matheus Rabelo <matheusfreirerabelo@gmail.com>
2017-11-10 13:12:09 -0200

    Add env port

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const zombies = require('./zombies.json');
+const [zombie] = require('./zombies.json');
 const express = require('express');
 const morgan = require('morgan');
+const port = process.env.PORT || 8080;
+const app = express();
 
-zombies.forEach(zombie => {
-    const app = express();
-    app.use(morgan('tiny'));
-    zombie.routes.forEach(route => {
-        const handler = (request, response) => {
-            response.set(route.response.header)
-                    .status(route.response.status)
-                    .json(route.response.body);
-        };
+app.use(morgan('tiny'));
+zombie.routes.forEach(route => {
+    const handler = (request, response) => {
+        response.set(route.response.header)
+                .status(route.response.status)
+                .json(route.response.body);
+    };
 
-        const method = route.method.toLowerCase();
-        app[method](route.path, handler);
-    });
-    app.listen(zombie.port, console.log('Zombie ' + zombie.port + ' is walking'));
+    const method = route.method.toLowerCase();
+    app[method](route.path, handler);
 });
+
+app.listen(port, console.log('Zombie ' + port + ' is walking'));
